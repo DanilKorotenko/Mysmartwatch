e4ccc3b6b5809653b7f729e92a0552f496784fcb
ANTON2012\anton <antonkorotenko5@gmail.com>
2026-08-17 19:57:52 +0300

    added code style

--- sketch_aug17b/sketch_aug17b.ino
+++ sketch_aug17b/sketch_aug17b.ino
@@ -2,45 +2,39 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <TimeLib.h>
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET); //Declaration for the size,setting,etc. of the OLED
 
 void displayTime()
 {
   display.clearDisplay();
 
-  // Variables for each line of text/numbers
-  String title = "TIME";
+    // Variables for each line of text/numbers
+    String title = "TIME";
 
-  int hours = hour();
-  int minutes = minute();
-  int seconds = second();
+    int Hours = hour();
+    int Minutes = minute();
+    int Seconds = second();
 
   display.setTextColor(WHITE);
 
-  // "TIME" at (0,0), size 2
   display.setTextSize(2);
   display.setCursor(0,0);
   display.print(title);
 
-  // "16" at (0,18), size 5
   display.setTextSize(4);
   display.setCursor(0,18);
-  display.print(hours,10);
+  display.print(Hours,10);
 
-display.drawChar(52, 18, ':', WHITE, BLACK, 4);
+  display.drawChar(52, 18, ':', WHITE, BLACK, 4);
 
-
-  // "45" at (78,18), size 5
   display.setTextSize(4);
   display.setCursor(78,18);
-  display.print(minutes,10);
+  display.print(Minutes,10);
 
-  // Small "45" at (52,50), size 2
   display.setTextSize(2);
   display.setCursor(52,50);
-  display.print(seconds,10);
+  display.print(Seconds,10);
 
   display.display();
 
@@ -48,10 +42,11 @@ display.drawChar(52, 18, ':', WHITE, BLACK, 4);
 
 void setup() 
 {
-    Serial.begin(9600); // Start serial communication at 9600 baud
+  Serial.begin(9600); // Start serial communication at 9600 baud
   Serial.println("Debugging started...");
   setTime(16, 34, 0, 17, 8, 2026); // hour, min, sec, day, month, year
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
@@ -66,7 +61,7 @@ void loop()
   // Serial.print(m);
   // Serial.print(":");
   // Serial.println(s);
-displayTime();
+  displayTime();
   delay(1000);
 }
 
