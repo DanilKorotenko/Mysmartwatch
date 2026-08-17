e9c998d143fa78c3e9e66252743192baefa2ad0d
ANTON2012\anton <antonkorotenko5@gmail.com>
2026-08-17 19:45:55 +0300

    added ticking

--- sketch_aug17b/sketch_aug17b.ino
+++ sketch_aug17b/sketch_aug17b.ino
@@ -6,23 +6,16 @@
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
-void setup() {
-    Serial.begin(9600); // Start serial communication at 9600 baud
-  Serial.println("Debugging started...");
-  setTime(16, 34, 0, 17, 8, 2026); // hour, min, sec, day, month, year
-  Serial.begin(19200);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-
+void displayTime()
+{
   display.clearDisplay();
 
   // Variables for each line of text/numbers
   String title = "TIME";
-  String hours = "16";
-  String minutes = "45";
-  String seconds = "45";
+
+  int hours = hour();
+  int minutes = minute();
+  int seconds = second();
 
   display.setTextColor(WHITE);
 
@@ -34,7 +27,7 @@ void setup() {
   // "16" at (0,18), size 5
   display.setTextSize(4);
   display.setCursor(0,18);
-  display.print(hours);
+  display.print(hours,10);
 
 display.drawChar(52, 18, ':', WHITE, BLACK, 4);
 
@@ -42,26 +35,38 @@ display.drawChar(52, 18, ':', WHITE, BLACK, 4);
   // "45" at (78,18), size 5
   display.setTextSize(4);
   display.setCursor(78,18);
-  display.print(minutes);
+  display.print(minutes,10);
 
   // Small "45" at (52,50), size 2
   display.setTextSize(2);
   display.setCursor(52,50);
-  display.print(seconds);
+  display.print(seconds,10);
 
   display.display();
+
 }
 
-void loop() {
-  int h = hour();
-  int m = minute();
-  int s = second();
-  Serial.print("Current time: ");
-  Serial.print(h);
-  Serial.print(":");
-  Serial.print(m);
-  Serial.print(":");
-  Serial.println(s);
+void setup() 
+{
+    Serial.begin(9600); // Start serial communication at 9600 baud
+  Serial.println("Debugging started...");
+  setTime(16, 34, 0, 17, 8, 2026); // hour, min, sec, day, month, year
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+}
+
+void loop() 
+{
+  // Serial.print("Current time: ");
+  // Serial.print(h);
+  // Serial.print(":");
+  // Serial.print(m);
+  // Serial.print(":");
+  // Serial.println(s);
+displayTime();
   delay(1000);
 }
 
