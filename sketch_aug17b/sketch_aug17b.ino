212a3f471bf732e9670d9cec609d0e8b3bde8691
ANTON2012\anton <antonkorotenko5@gmail.com>
2026-08-17 18:38:40 +0300

    added time printing on serial monitor,but its brocken

--- sketch_aug17b/sketch_aug17b.ino
+++ sketch_aug17b/sketch_aug17b.ino
@@ -1,12 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
+#include <TimeLib.h>
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
 void setup() {
+    Serial.begin(9600); // Start serial communication at 9600 baud
+  Serial.println("Debugging started...");
+  setTime(16, 34, 0, 17, 8, 2026); // hour, min, sec, day, month, year
+  Serial.begin(19200);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
@@ -49,6 +53,15 @@ display.drawChar(52, 18, ':', WHITE, BLACK, 4);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int h = hour();
+  int m = minute();
+  int s = second();
+  Serial.print("Current time: ");
+  Serial.print(h);
+  Serial.print(":");
+  Serial.print(m);
+  Serial.print(":");
+  Serial.println(s);
+  delay(1000);
 }
 
