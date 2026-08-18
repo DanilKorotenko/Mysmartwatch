8bce8661c11afa77e9f1535ed9b715443ba4e02f
ANTON2012\anton <antonkorotenko5@gmail.com>
2026-08-18 18:24:30 +0300

    """added""" stop watch

---
+++ test_copy17a/test_copy17a.ino
@@ -0,0 +1,90 @@
+unsigned long OldestMillis;
+unsigned long NewMillis;
+const int buttonPin = 2;  // Pin connected to button
+int buttonState = 0;      // Variable to store button state
+int ButtonPressed = 0
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TimeLib.h>
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET); //Declaration for the size,setting,etc. of the OLED
+
+void displayTime()
+{
+  display.clearDisplay();
+
+    // Variables for each line of text/numbers
+    String title = "TIME";
+
+    int Hours = hour();
+    int Minutes = minute();
+    int Seconds = second();
+
+  display.setTextColor(WHITE);
+
+  display.setTextSize(2);
+  display.setCursor(0,0);
+  display.print(title);
+
+  display.setTextSize(4);
+  display.setCursor(0,18);
+  display.print(Hours,10);
+
+  display.drawChar(52, 18, ':', WHITE, BLACK, 4);
+
+  display.setTextSize(4);
+  display.setCursor(78,18);
+  display.print(Minutes,10);
+
+  display.setTextSize(2);
+  display.setCursor(52,50);
+  display.print(Seconds,10);
+
+  display.display();
+
+}
+
+void setup() 
+{
+  pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor
+  Serial.begin(9600); // Start serial communication at 9600 baud
+  Serial.println("Debugging started...");
+  setTime(16, 34, 0, 17, 8, 2026); // hour, min, sec, day, month, year
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+}
+
+void loop() 
+{
+  buttonState = digitalRead(buttonPin);
+  OldestMillis = millis();
+
+  if (buttonState == LOW)
+  {
+    ButtonPressed = 1;
+  } else;
+  
+{
+  
+}
+  if (ButtonPressed = 1)
+  {
+    NewMillis = OldestMillis - NewMillis;
+  }
+
+  // Serial.print("Current time: ");
+  // Serial.print(h);
+  // Serial.print(":");
+  // Serial.print(m);
+  // Serial.print(":");
+  // Serial.println(s);
+  displayTime();
+  Serial.println(NewMillis);
+  delay(1000);
+}
+
